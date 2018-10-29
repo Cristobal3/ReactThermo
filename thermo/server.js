ca0be2c35a57892b7622659a6b5e5af8045182b4
Cristobal <manzanares.cristobal@gmail.com>
2018-10-29 15:25:54 -0500

    Added production folder, corrected warnings on three files.

--- thermo/server.js
+++ thermo/server.js
@@ -4,6 +4,7 @@ const db = require('./db/insert')
 const app = express();
 const port = process.env.PORT || 5000;
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
